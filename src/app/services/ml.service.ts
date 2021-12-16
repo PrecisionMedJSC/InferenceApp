7e64594f649387cfefa31f08b552a9a8c70e1a95
TamNguyenMinh <tamnguyenminhse@gmail.com>
2021-12-16 15:32:12 +0700

    change endpoint

--- src/app/services/ml.service.ts
+++ src/app/services/ml.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +8,7 @@ import { Injectable } from '@angular/core';
 export class MlService {
 
   // env = 'https://ep.diabetes.precisionmed.dev';
-  env = "http://127.0.0.1:8000";
+  env = environment.endpoint;
   constructor(private httpClient: HttpClient) { }
 
   public getModels(modelName: string) {
